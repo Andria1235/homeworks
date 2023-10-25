05049107ee702ad068bd0956b61d3ff0279bb2a9
Andria1235 <146097484+Andria1235@users.noreply.github.com>
2023-10-25 21:08:11 +0400

    Add files via upload

---
+++ group 102/lesson1.py
@@ -0,0 +1,25 @@
+name ="andria" #ეს არის string ტიპის ცვლადი
+age ="22" #ეს არის int (integer) მთელი რიცხვი
+heigh ="185.5" #ეს არის float ტიპის ცვლადი (ათწილადი)
+#name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+# "andria" არის ცვლადისთვის მნიშვნელობა
+surname ="tughushi"
+
+print (name)
+print (surname)
+#print ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+knows_programming = True #true ან false
+is_ugly = False #snakecase უბრალოდ წერის სტილი სტანდარტულად
+
+isugly = False #ჯავასკრიფტული qamelcase
+
+
+print(name +" "+ surname)
+print(name + age)
+print(type(age))
+print(type(surname))
+print(type(name))
+print(name + str(age))
+print(name +" "+ str(age))
---
+++ group 102/myi information.py
@@ -0,0 +1,4 @@
+name = ("hi, my name is andria")
+surname = ("tughushi")
+age = ("im 14 years old")
+print (name +surname+" " +age)
