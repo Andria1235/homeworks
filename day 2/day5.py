60449a097d768d49d4ff21d48444a1220bd6b9b6
Andria1235 <146097484+Andria1235@users.noreply.github.com>
2023-10-30 19:42:10 +0400

    Add files via upload

---
+++ day 2/day5.py
@@ -0,0 +1,15 @@
+i=0
+while True:
+    i+=1
+    if i==1:
+        continue
+    if i==2:
+        continue
+    if i==3:
+        continue
+    if i==14:
+        break
+    print(i)
+
+print("ჯამი 250 ლარი")
+    
