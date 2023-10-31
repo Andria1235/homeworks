8d56fc35d2e12fb429d93491bace66216c5ac644
Andria1235 <146097484+Andria1235@users.noreply.github.com>
2023-10-31 23:29:59 +0400

    Add files via upload

---
+++ day 2/homeworks/day8.py
@@ -0,0 +1,4 @@
+family= ("ana","40","50","giorgi","41","51","nini","17","27")
+fml="my mothers name is {}, her age is {},her age in ten years will be {}, my fathers name is {},his age is{},his age in ten years will be {}, my sisters name is {}, her age is {}, her age in ten years will be {} ".format (family [0],family [1],family [2],family[3],family[4],family[5],family[6],family[7],family[8])
+
+print(fml)
